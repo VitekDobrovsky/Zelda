f9213e9bff7daafd2f037d8be1d0987bb6de944c
Vitek <vit.dobrovsky@seznam.cz>
2022-04-16 21:09:22 +0200

    added camera and overlaping

--- level.py
+++ level.py
@@ -12,7 +12,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # Sprite Group set up
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         # Sprite setup
@@ -30,6 +30,24 @@ class Level:
 
     def run(self):
         # Update and Draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # General setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(100,200)
+
+    def custom_draw(self,player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
