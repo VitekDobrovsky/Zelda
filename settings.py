2064366305dbace79d8902ea60a0fbfd145ac851
Vitek <vit.dobrovsky@seznam.cz>
2022-04-28 18:24:25 +0200

    added better ui

--- settings.py
+++ settings.py
@@ -25,8 +25,8 @@ UI_BORDER_COLOR_ACTIVE = 'gold'
 
 # weapons
 weapon_data = {
-    'sword': {'cooldown': 100, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
-    'lance': {'cooldown': 400, 'damage': 30,'graphic':'../graphics/weapons/lance/full.png'},
-    'axe': {'cooldown': 300, 'damage': 20, 'graphic':'../graphics/weapons/axe/full.png'},
-    'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'../graphics/weapons/rapier/full.png'},
-    'sai':{'cooldown': 80, 'damage': 10, 'graphic':'../graphics/weapons/sai/full.png'}}
+    'sword': {'cooldown': 100, 'damage': 15,'graphic':'graphics/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30,'graphic':'graphics/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'graphic':'graphics/weapons/axe/full.png'},
+    'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'graphics/weapons/rapier/full.png'},
+    'sai':{'cooldown': 80, 'damage': 10, 'graphic':'graphics/weapons/sai/full.png'}}
--- ui.py
+++ ui.py
@@ -11,6 +11,13 @@ class UI:
         self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+        # weapon
+        self.weapon_graphic = []
+        for weapon in weapon_data.values():
+            path = weapon['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphic.append(weapon)
+
     def show_bars(self,current,max,bg_rect,color):
         # draw bg
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
@@ -24,6 +31,34 @@ class UI:
         pygame.draw.rect(self.display_surface,color,current_rect)
         pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
 
+    def show_exp(self,exp):
+        text_surf = self.font.render(str(int(exp)),False,TEXT_COLOR)
+        text_rect = text_surf.get_rect(bottomright = (self.display_surface.get_size()[0] - 20,self.display_surface.get_size()[1] - 20))
+
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,text_rect.inflate(10,10))
+        self.display_surface.blit(text_surf,text_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, text_rect.inflate(10, 10),3)
+
+    def selection_box(self,left,top,switched):
+        bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+        if switched:
+            pygame.draw.rect(self.display_surface, UI_BORDER_COLOR_ACTIVE, bg_rect, 3)
+        else:
+            pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
+        return bg_rect
+
+    def weapon_overlay(self,weapon_index,switched):
+        bg_rect = self.selection_box(10,630,switched)
+        weapon_surf = self.weapon_graphic[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center = bg_rect.center)
+
+        self.display_surface.blit(weapon_surf,weapon_rect)
+
     def display(self,player):
         self.show_bars(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
         self.show_bars(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
+        self.show_exp(player.exp)
+
+        self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
+        #self.selection_box(80,635)
